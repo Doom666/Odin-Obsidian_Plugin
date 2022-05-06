c2e11817f0c212a8ba5f176c613bce9349708b72
CarlosMK <kaahrl@gmail.com>
2022-05-06 15:14:28 +0200

    Done testing

--- main.ts
+++ main.ts
@@ -20,13 +20,14 @@ export default class MyPlugin extends Plugin {
 		const ribbonIconEl = this.addRibbonIcon('dice', 'Odin Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
+			new Notice('Almost like print!');
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		statusBarItemEl.setText('Odin Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
